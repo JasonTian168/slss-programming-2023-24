7a2f249024721d70d8d86ae7afe61eb8c68dd332
Jason Tian <jasontian1105@gmail.com>
2023-10-27 09:43:15 -0700

    Added

---
+++ Notes/Types.md
@@ -0,0 +1,19 @@
+In Python, data can be grouped in categories in types.
+
+| Name     | Example       |
+| ---      | ---           |
+| string   |  "hello"      |
+| list     | [1, 2, 3, 4]  |
+| int      | 1, 2, 3, 4    |
+| float    | 3.5, -3.5, 1.0|
+| boolean  | True or False |
+
+
+
+
+An example pf using these types of data:
+
+```python
+favourite_food = "tempura"
+my_age = 16
+```
--- Notes/food_suggestion_bot.py
+++ Notes/food_suggestion_bot.py
@@ -12,17 +12,10 @@ import time
 
 # Introduce the bot to the user
 # Ask the user what their favourite food is
-print("Hello, I am a bot here to recommend a resturant")
-time.sleep(1)
-fav_food = input("To help me suggest a resturant, can you tell me what your favourite food?")
-time.sleep(1)
 
-# Italian Cuisisne
-italian_food = ["pasta", "pizza", "cannelont", "tiramisu"]
-# If they answer Italian with Italian food, suggest an Italian resturant.
-if fav_food.lower().strip("!.,?") in italian_food:
-    print("I see that you may like Italian food.")
-    time.sleep(1)
-    print("I suggestThe Old Spaghetti Factory. The address is: 14200 Entertainment Blvd #110, Richmond, BC V6W 1K3 ")
-else:
-    print("Sorry I unfortunatly don't know what type of food you replied is")
+
+
+
+ubials_inventory = ["sword", "shield", "flask", "phone", "bigger sword"] 
+ 
+print("bigger sword" in ubials_inventory) 
