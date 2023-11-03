38779eafab549e545e42be932d9e635d21eea431
Jason Tian <jasontian1105@gmail.com>
2023-11-03 09:26:27 -0700

    Added

---
+++ Notes/mcdonalds.py
@@ -0,0 +1,15 @@
+print("Hi, I am a Mcdonald AI servant")
+
+print("Would you like a burger for $5 (yes/no)")
+burger_choice = input().strip(",.?!").lower()
+
+print("Would you like fries for $3 (yes/no)")
+fries_choice = input().strip(",.?!").lower()
+sum = 0
+
+if burger_choice == "yes":
+    sum += 5
+if fries_choice == "yes":
+    sum += 3
+
+print("Your total would be " + str(sum * 1.12))
