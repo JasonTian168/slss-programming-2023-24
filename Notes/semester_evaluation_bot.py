81cecf7a4b31c814c4c6d4550b49cf94370eeb7e
Jason Tian <jasontian1105@gmail.com>
2023-11-06 09:38:36 -0800

    Added

---
+++ Notes/semester_evaluation_bot.py
@@ -0,0 +1,18 @@
+current_courses = input("How many courses are you taking right now?")
+NUMS_RESPONDENDTS = current_courses
+course_num = 1
+sum = 0
+
+for _ in range(int(NUMS_RESPONDENDTS)):
+    course_evaluation = input("What would you rate course" + str(course_num) + " out of 5?")
+    course_num += 1
+    sum += int(course_evaluation)
+
+course_average = sum / int(NUMS_RESPONDENDTS)
+
+if course_average <= 1:
+    print("Ouch")
+elif course_average > 1 and course_average < 3:
+    print("Not a bad semester")
+else:
+    print("Glad to hear that!")
